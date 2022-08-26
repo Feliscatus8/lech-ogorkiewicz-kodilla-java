bf451159e3174e73f0e6d7ab24abfe871761fc10
Lech Ogórkiewicz <feliscatus@interia.eu>
2022-08-26 17:36:57 +0200

    7.1 added PoemBeautifier class

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,34 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionCalculator;
+
+public class StreamMain {
+    public static void main(String[] args){
+        System.out.println("Welcome to module 7 - Stream\n");
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("This is an example text", (text) -> "ABC " + text + " ABC");
+        poemBeautifier.beautify("This is an example text", (text) -> text.toUpperCase());
+        poemBeautifier.beautify("This is an example text", (text) -> text.replace('s', 'n'));
+        poemBeautifier.beautify("This is an example text", (text) -> text.replaceAll("text", "meow"));
+
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("\nThis is an example text"));
+
+        System.out.println("\nCalculating expressions with lambdas");
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("\nCalculating expressions with method references");
+        expressionExecutor.executeExpression(8, 2, FunctionCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(8, 2, FunctionCalculator::addAToB);
+        expressionExecutor.executeExpression(8, 2, FunctionCalculator::subtractAFromB);
+        expressionExecutor.executeExpression(8,2,FunctionCalculator::divideAByB);
+    }
+}
