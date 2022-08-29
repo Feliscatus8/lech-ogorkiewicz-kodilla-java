272bf48875dfb02c8c962578d852faebe9caefd8
Lech Ogórkiewicz <feliscatus@interia.eu>
2022-08-29 19:29:25 +0200

    8.3 implemented SecondChallenge

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,2 +1,14 @@
-package com.kodilla.exception.test;public class ExceptionHandling {
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try{
+            System.out.println(secondChallenge.probablyIWillThrowException(0, 1));
+        }catch (Exception e){
+            System.out.println("Exception ocurred: " + e);
+        }finally {
+            System.out.println("Exception or not, here i am");
+        }
+    }
 }
--- kodilla-exception/src/test/java/com/kodilla/exception/test/SecondChallengeTestSuite.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/SecondChallengeTestSuite.java
@@ -1,2 +1,21 @@
-package com.kodilla.exception.test;public class SecondChallengeTestSuite {
+package com.kodilla.exception.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SecondChallengeTestSuite {
+    @Test
+    void testProbabliIWillThrowAnException(){
+        //Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+        //When & Then
+        assertAll(
+                () ->assertDoesNotThrow(() -> secondChallenge.probablyIWillThrowException(1, 1)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(0.5, 2)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(4, 8)),
+                () -> assertThrows(Exception.class, () -> secondChallenge.probablyIWillThrowException(1.5, 1.5))
+        );
+    }
 }
